fe2e117f623e402c5e72114c8b9703d4e56c9802
shangweilin <shangweilin7@gmail.com>
2017-12-04 01:48:24 +0800

    add annotation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
   end
